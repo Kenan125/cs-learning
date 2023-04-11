d437a3ee45bbb97b217c56610a928e365ce2465b
Kanan Ismayilzada <kenanismayilzade12@gmail.com>
2023-04-11 19:59:12 +0300

    Add project files.

---
+++ trycatch/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace trycatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number!");
+            string userInput = Console.ReadLine();
+
+
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format exception, please enter the correct type next time.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Overflow exception, the number was too long or too short for an int32");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("ArgumentNullException, the value was empty(null)");
+            }
+            finally
+            {
+                Console.WriteLine("This is called anyways!");
+            }
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
